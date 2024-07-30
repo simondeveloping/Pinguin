da6e67d08797909dbdf089719e4e98b9ef60b438
Pham <yv521464@win.rz.rwth-aachen.de>
2024-07-30 15:49:22 +0200

    added new feature

--- script.js
+++ script.js
@@ -8,7 +8,35 @@ function change() {
     b.style.left = i + 'px';
     b.style.top = j + "px";
 }
-
+document.getElementById("button1").addEventListener("click",()=>{
+    const defaults = {
+        spread: 360,
+        ticks: 100,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["heart"],
+        colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+      };
+      
+      confetti({
+        ...defaults,
+        particleCount: 50,
+        scalar: 2,
+      });
+      
+      confetti({
+        ...defaults,
+        particleCount: 25,
+        scalar: 3,
+      });
+      
+      confetti({
+        ...defaults,
+        particleCount: 10,
+        scalar: 4,
+      });
+})
 document.getElementById("button1").addEventListener("click", function () {
     document.getElementById("textbox").textContent = "Yeey Date time :D";
     pic.src = "pinguin.jpg";
